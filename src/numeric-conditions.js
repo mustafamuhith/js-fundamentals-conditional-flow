1e2cf8c351ef8447a752e6bc0da9de02521eadb1
George Roth <g@georgeroth.eu>
2022-09-12 15:01:47 +0100

    Adding Numeric Conditions

--- src/numeric-conditions.js
+++ src/numeric-conditions.js
@@ -3,25 +3,48 @@
 const ARRAY_ONE = []
 const ARRAY_TWO = ['Hello', 'Conditions']
 
-// 1. Use conditional statements to set answerOne to false if ARRAY_ONE is not empty
+// 1. Use conditional statements to say if ARRAY_ONE is not empty
+// then set answerOne to false 
 // or true if it is empty
 let answerOne
 
-// 2. Use conditional statements to set answerTwo to false if ARRAY_TWO is not empty
-// or true if it is empty
+if (ARRAY_ONE != []) {
+  answerOne = false
+} else if (ARRAY_ONE === []) {
+  answerOne = true
+}
+
+// 2. Use conditional statements if ARRAY_TWO is not empty 
+// then set answerTwo to false 
+// if ARRAY_TWO is empty set answerTwo to true 
 let answerTwo
+
+if (ARRAY_TWO != []) {
+  answerTwo = false
+} else if (ARRAY_TWO === []) {
+  answerTwo = true
+}
 
 const NUM_ONE = 92837
 const NUM_TWO = 92827
 
-// 3. Use conditional statements to set answerThree to true if NUM_ONE is more than NUM_TWO
+// 3. Use conditional statements to set answerThree to true
+// if NUM_ONE is more than NUM_TWO
 let answerThree
+
+if (NUM_ONE > NUM_TWO) {
+  answerThree = true
+}
 
 const NUMBERS = [67, 2039, 17, 501, 98, 139, 21]
 
 // 4. Use a combination of a loop and conditional statements to set answerFour
 // to the lowest number in the NUMBERS array
 let answerFour
+
+for (i in NUMBERS) {
+  
+}
 
 // Don't edit the code below this line
 module.exports = {
